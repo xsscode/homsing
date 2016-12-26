bc7b60ef5fba40e7844b4719f32219aae4dd9184
xsscode <darkeye12.com>
2016-12-26 16:59:01 +0800

    修改server

--- server.js
+++ server.js
@@ -1,33 +1,30 @@
 /**
  * Created by Administrator on 2016/12/10 0010.
  */
-var express=require("express")
-var path=require("path")
-var b=express()
-var x=path.join(__dirname,'view')
-var y=path.join(__dirname,"public")
-b.use("/",express.static(x))
-b.use("/public",express.static(y))
-<<<<<<< HEAD:server.js
-b.listen(16933,console.log(
-=======
-b.listen(8888,console.log(
->>>>>>> 6039854cda9ab3a2b2c846e8b1703521c310e444:app.js
-	"sever"
-))
+var express=require("express");
+var path=require("path");
+var b=express();
+var sha1=require('sha1');
 var proxy=require('http-proxy-middleware');
+var x=path.join(__dirname,'view');
+var y=path.join(__dirname,"public");
+b.use("/",express.static(x));
+b.use("/public",express.static(y));
+
+b.listen(16933,function(){
+	console.log('server run at port 16933')
+});
 
 
-b.use("/api",proxy({
-	target:"http://guanjp.com:9805",
-	changeOrigin:true,
-	ws:true,
-	cookieRewrite:true,
-	pathRewrite:{
-	"^/api":"/"
-}
-<<<<<<< HEAD:server.js
-}))
-=======
-}))
->>>>>>> 6039854cda9ab3a2b2c846e8b1703521c310e444:app.js
+
+b.use("/api",proxy( {
+	target: "http://guanjp.com:9805",
+	changeOrigin: true,
+	ws: true,
+	cookieRewrite: true,
+	pathRewrite: {
+		"^/api": "/"
+	}
+}));
+module.exports=b;
+
