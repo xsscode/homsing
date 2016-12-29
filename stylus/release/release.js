3c3ad2cc75be4b91f1f991d52c81d524c38589d2
xsscode <darkeye12.com>
2016-12-29 16:18:59 +0800

    dt

--- stylus/release/release.js
+++ stylus/release/release.js
@@ -20,79 +20,79 @@ $(".type-li").on("click",function(){
 	self.find(".l" ).addClass("img-p")
 })
 var bbt=$(".btn-bottom");
-bbt.on("tap",function(){
+bbt.on("click",function(){
 	$(".btn-art").css("display","none")
 	$(".btn-bg").css("display","none")
 })
 
-$(".boot-btn").on("tap",function(){
+$(".boot-btn").on("click",function(){
 	$(".btn-bg").fadeIn(300)
 	$(".btn-art").fadeIn(300)
 })
 
-$(".log-more").on("tap",function(){
+$(".log-more").on("click",function(){
 	console.log("gggggggg")
 	$(this ).find(".jj" ).toggleClass("log-img")
 })
 
-$(".nav-l").on("tap",function(){
+$(".nav-l").on("click",function(){
 	$(".btn-bg").fadeIn(300)
 	$(".rele-out").fadeIn(300)
 })
 
-$(".pre-resours").on("tap",function(){
+$(".pre-resours").on("click",function(){
 	console.log(66)
 	location.href = "./release.html"
 })
 
-$(".b-l").on("tap",function(){
+$(".b-l").on("click",function(){
 	$(".rele-out" ).fadeOut(300)
 	$(".btn-bg" ).fadeOut(300)
 	$(".dem-bg").fadeOut(300)
 	$(".dem-out").fadeOut(300)
 })
-$(".b-r").on("tap",function(){
+$(".b-r").on("click",function(){
 	console.log(668)
 	location.href = "./release-1.html"
 });
-$(".re-box-l").on("tap",function(){
+$(".re-box-l").on("click",function(){
 	console.log(66)
 	location.href = "./release.html"
 })
-$(".re-box-r").on("tap",function(){
+$(".re-box-r").on("click",function(){
 	console.log(66)
 	location.href = "./dem-relse.html"
 })
-$(".can").on("tap",function(){
+$(".can").on("click",function(){
 	console.log(66)
 	$(".re-bg" ).hide()
 	$(".re-box" ).hide()
 	location.href = "../home/home.html"
 })
 
-$(".h-left").on("tap",function(){
+$(".h-left").on("click",function(){
 	$(".dem-bg").fadeIn(300)
 	$(".dem-out").fadeIn(300)
 
 })
-$(".h-right").on("tap",function(){
+$(".h-right").on("click",function(){
 	location.href = "./dem-com.html"
 })
-$(".en-bot").on("tap",function(){
+$(".en-bot").on("click",function(){
 	location.href = "./release.html"
 })
-$(".en-bot1").on("tap",function(){
+$(".en-bot1").on("click",function(){
 	location.href = "./release-1.html"
 })
-$(".head-add").on("tap",function(){
+$(".head-add").on("click",function(){
 	location.href = "./new-resours.html"
 })
-$(".sec-submit").on("tap",function(){
+$(".sec-submit").on("click",function(){
 	location.href = "./submit-com.html"
 })
-$(".head-search").on("tap",function(){
+$(".head-search").on("click",function(){
 	location.href = "./release-search.html"
 })
-$(".nav-text").on("tap",function(){
+$(".nav-text").on("click",function(){
 	location.href = "./release.html"
 })
